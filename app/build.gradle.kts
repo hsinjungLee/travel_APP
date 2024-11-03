77503266dff79ff8a6bfd5720c5d7a119399f13c
hsinjungLee <144487630+hsinjungLee@users.noreply.github.com>
2024-11-03 13:38:10 +0800

    完成地圖（僅顯示）

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     implementation("com.google.android.gms:play-services-maps:19.0.0")
+
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
