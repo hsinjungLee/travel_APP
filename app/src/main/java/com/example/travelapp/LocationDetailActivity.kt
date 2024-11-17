d2b9467193ddc2e972e7558611600b262dabfbf0
hsinjungLee <144487630+hsinjungLee@users.noreply.github.com>
2024-11-17 16:15:35 +0800

    新增delete Button 刪除地點

--- app/src/main/java/com/example/travelapp/LocationDetailActivity.kt
+++ app/src/main/java/com/example/travelapp/LocationDetailActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.TimePicker
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
 
 class LocationDetailActivity : AppCompatActivity() {
 
@@ -13,15 +14,17 @@ class LocationDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location_detail)
 
+        // 获取传递的地点坐标和名称
         val locationCoordinates = intent.getStringExtra("location_coordinates") ?: "Unknown"
         val locationName = intent.getStringExtra("location_name") ?: "Location Name"
 
         val locationTextView: TextView = findViewById(R.id.locationNameTextView)
-
         locationTextView.text = locationName
 
         val timePicker: TimePicker = findViewById(R.id.timePicker)
         val saveButton: Button = findViewById(R.id.saveButton)
+        val deleteButton: Button = findViewById(R.id.deleteButton)
+
 
         saveButton.setOnClickListener {
             val hour = timePicker.hour
@@ -32,6 +35,27 @@ class LocationDetailActivity : AppCompatActivity() {
             val editor = sharedPreferences.edit()
             editor.putString("saved_time_for_${locationCoordinates}", time)
             editor.apply()
+
+
+            val intent = Intent(this, SavedLocationsActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        deleteButton.setOnClickListener {
+            val sharedPreferences = getSharedPreferences("maps_pref", MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+
+
+            editor.remove("saved_time_for_${locationCoordinates}")
+            val savedLocationsSet = sharedPreferences.getStringSet("saved_locations", mutableSetOf()) ?: mutableSetOf()
+            savedLocationsSet.remove(locationCoordinates)
+            editor.putStringSet("saved_locations", savedLocationsSet)
+            editor.apply()
+
+
+            Toast.makeText(this, "Location Deleted", Toast.LENGTH_SHORT).show()
+
 
             val intent = Intent(this, SavedLocationsActivity::class.java)
             startActivity(intent)
